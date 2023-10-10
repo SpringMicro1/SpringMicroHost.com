6b1eb028cb0473a927cd834972d8d67ca1f4d301
David Buckley <runalldayandnight@gmail.com>
2023-10-10 14:21:21 -0600

    Upgrade astro v3, coupon redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
   site: "https://www.springmicrohost.com/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-    react(),
-  ],
+  integrations: [tailwind(), mdx(), sitemap(), react()],
   vite: {
     ssr: {
       noExternal: ["react-icons"],
     },
   },
+  adapter: vercel(),
 });
